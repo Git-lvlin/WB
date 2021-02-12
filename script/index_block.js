8f316193ddf5f04d6ef896191aeaad7338bdb4a7
Git-lvlin <1172383239@qq.com>
2021-02-12 11:00:05 +0800

    大年初一

--- script/index_block.js
+++ script/index_block.js
@@ -3,12 +3,12 @@ window.addEventListener('load',function(){
     var box_con=$2('.box_con');
     var par=0;
     console.log(spa)
-    for(let i=0;i<lit.length;i++){
-        lit[i].onmouseenter=function(){
+    for(let i=0;i<spa.length;i++){
+        spa[i].onmouseenter=function(){
             box_con[par].style.display='none';//上次的隐藏
-            lit[par].children[0].className='';//清除上次的样式
+            spa[par].children[0].className='';//清除上次的样式
 
-            lit[i].children[0].className='ative';//给当前添加类名
+            spa[i].children[0].className='ative';//给当前添加类名
             box_con[i].style.display='block';//当前的显示
             par=i;
 
--- script/w_pt_nav.js
+++ script/w_pt_nav.js
@@ -21,30 +21,24 @@ $(function () {
                     }
                     col_ul.innerHTML = domli;
                 }
-
+            }
+            // 图片文字切换
+            var con_li = $2('.col_1 ul li');
+            var con_img = $2('.col_1 img');
+            var con_p = $2('.col_1 p');
+            for (let i = 0; i < con_li.length; i++) {
+                con_li[i].onmouseenter = function () {
+                    con_img[i].style.display = 'none'; //图片隐藏
+                    con_p[i].style.display = 'block'; //文字显示
+                }
+                con_li[i].onmouseleave = function () {
+                    con_img[i].style.display = 'block'; //图片显示
+                    con_p[i].style.display = 'none'; //文字隐藏
+                }
             }
         }
     })
-    console.log(col_ul)
-    console.log(a);
-    var b = new Promise(_=>_)
-    console.log(b.__proto__);
 
-
-    // 图片文字切换
-    var con_li = $2('.col_1 ul li');
-    var con_img = $2('.col_1 img');
-    var con_p = $2('.col_1 p');
-    for (let i = 0; i < con_li.length; i++) {
-        con_li[i].onmouseenter = function () {
-            con_img[i].style.display = 'none'; //图片隐藏
-            con_p[i].style.display = 'block'; //文字显示
-        }
-        con_li[i].onmouseleave = function () {
-            con_img[i].style.display = 'block'; //图片显示
-            con_p[i].style.display = 'none'; //文字隐藏
-        }
-    }
 
 
 })
